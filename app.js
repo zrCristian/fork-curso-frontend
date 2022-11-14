89cf94fc3d78c122bc55c14a1e11f1557b7064b7
Cristian Zamora <cristianelnum10@gmail.com>
2022-11-13 22:28:10 -0300

    Menu mobile despegable

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const body = document.querySelector("body")
 
-navbarEmail.addEventListener("click", () => desktopMenu.classList.toggle("active"))
+navbarEmail.addEventListener("click", () => desktopMenu.classList.toggle("inactive"))
+
+const menu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+menu.addEventListener("click", () => mobileMenu.classList.toggle("inactive"))
+
+console.log(mobileMenu)
 
