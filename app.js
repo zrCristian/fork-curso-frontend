94f6af4c24c7fc356fe3bc2cacf95266f669ff62
Cristian Zamora <cristianelnum10@gmail.com>
2022-11-14 00:49:59 -0300

    carrito de compras despegable terminado

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
-const body = document.querySelector("body")
-
-navbarEmail.addEventListener("click", () => desktopMenu.classList.toggle("inactive"))
-
 const menu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const carritoIcon = document.querySelector(".navbar-shopping-cart")
+const  asideCarrito= document.querySelector(".product-detail")
 
-menu.addEventListener("click", () => mobileMenu.classList.toggle("inactive"))
+navbarEmail.addEventListener("click", () => {
+    desktopMenu.classList.toggle("inactive")
+    if (!asideCarrito.classList.contains("inactive")) asideCarrito.classList.add("inactive")
 
-console.log(mobileMenu)
+})
+
+menu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("inactive")
+    asideCarrito.classList.add("inactive")
+
+})
+
+carritoIcon.addEventListener("click", () => {
+    asideCarrito.classList.toggle("inactive")
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+})
 
