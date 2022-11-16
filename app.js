6e4647f8c84ac6d1da31a8b741c1a3c03349f642
Cristian Zamora <cristianelnum10@gmail.com>
2022-11-16 01:47:30 -0300

    haciendo interactivo los detalles de los productos al dar click. Ademas de ser compatibles con los demas componentes

--- app.js
+++ app.js
@@ -1,90 +1,106 @@
-const navbarEmail = document.querySelector(".navbar-email")
+const navEmail = document.querySelector(".navbar-email")
+const menuMobileIcon = document.querySelector(".menu")
 const desktopMenu = document.querySelector(".desktop-menu")
-const menu = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-const carritoIcon = document.querySelector(".navbar-shopping-cart")
-const  asideCarrito = document.querySelector(".carritoContainer")
+const cartIcon = document.querySelector(".navbar-shopping-cart")
+const productDetailCloseIcon = document.querySelector(".product-detail-close")
+const cartAsideContainer = document.querySelector("#carritoContainer")
+const productDetailAside = document.querySelector("#productosDetalles")
 const cardsContainer = document.querySelector(".cards-container")
 
-navbarEmail.addEventListener("click", () => {
+
+navEmail.addEventListener("click", toggleDesktopMenu)
+menuMobileIcon.addEventListener("click", toggleMobileMenu)
+cartIcon.addEventListener("click", toggleCartAside)
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside)
+
+
+function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive")
-    if (!asideCarrito.classList.contains("inactive")) asideCarrito.classList.add("inactive")
+    if (!cartAsideContainer.classList.contains("inactive")) cartAsideContainer.classList.add("inactive")
+    if (!productDetailAside.classList.contains("inactive")) productDetailAside.classList.add("inactive")
 
-})
+}
 
-menu.addEventListener("click", () => {
+function toggleMobileMenu () {
     mobileMenu.classList.toggle("inactive")
-    asideCarrito.classList.add("inactive")
+    if (!cartAsideContainer.classList.contains("inactive")) cartAsideContainer.classList.add("inactive")
+    if (!productDetailAside.classList.contains("inactive")) productDetailAside.classList.add("inactive")
+}
 
-})
+function toggleCartAside () {
+    cartAsideContainer.classList.toggle("inactive")
+    if (!desktopMenu.classList.contains("inactive")) desktopMenu.classList.add("inactive")
+    if (!mobileMenu.classList.contains("inactive")) mobileMenu.classList.add("inactive")
+    if (!productDetailAside.classList.contains("inactive")) productDetailAside.classList.add("inactive")
+}
 
-carritoIcon.addEventListener("click", () => {
-    asideCarrito.classList.toggle("inactive")
-    mobileMenu.classList.add("inactive")
-    desktopMenu.classList.add("inactive")
-})
+function openProductDetailAside () {
+    productDetailAside.classList.remove("inactive")
+    if (!cartAsideContainer.classList.contains("inactive")) cartAsideContainer.classList.add("inactive")
+    if (!mobileMenu.classList.contains("inactive")) mobileMenu.classList.add("inactive")
+    if (!desktopMenu.classList.contains("inactive")) desktopMenu.classList.add("inactive")
+}
 
+function closeProductDetailAside () {
+    productDetailAside.classList.add("inactive")
+}
 
 
 const listaProductos = [];
 listaProductos.push({
     name: "Bike",
     price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: "https://static01.nyt.com/images/2020/11/03/obituaries/25maradona-ES-1/00Maradona-mediumSquareAt3X.jpg",
 })
-
 listaProductos.push({
     name: "celular",
     price: 400,
     image: "https://i.imgur.com/HA2Bu.jpg",
 
 })
-
 listaProductos.push({
     name: "computadora",
     price: 700,
-    image: "https://w0.peakpx.com/wallpaper/205/620/HD-wallpaper-messi-cute-messi-cute-argentina-messi-2021-messi-argentina-messi-2018-messi-argentina.jpg"
+    image: "https://wallpaperaccess.com/full/2760068.jpg"
 })
 listaProductos.push({
     name: "Bike",
     price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: "https://i.pinimg.com/originals/d3/f7/8b/d3f78b9a5c73ebaa5159c33c5e58b3c5.jpg",
 })
-
 listaProductos.push({
     name: "celular",
     price: 400,
-    image: "https://i.imgur.com/HA2Bu.jpg",
+    image: "https://wallpapercave.com/wp/wp3107785.jpg",
 
 })
-
 listaProductos.push({
     name: "computadora",
     price: 700,
-    image: "https://w0.peakpx.com/wallpaper/205/620/HD-wallpaper-messi-cute-messi-cute-argentina-messi-2021-messi-argentina-messi-2018-messi-argentina.jpg"
+    image: "https://wallup.net/wp-content/uploads/2019/09/633066-leo-messi-hombre-futbolista-f-c-barcelona-argentina-748x496.jpg"
 })
 listaProductos.push({
     name: "Bike",
     price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: "https://w0.peakpx.com/wallpaper/195/560/HD-wallpaper-argentina-campeon-copa-america-messi.jpg",
 })
-
 listaProductos.push({
     name: "celular",
     price: 400,
-    image: "https://i.imgur.com/HA2Bu.jpg",
+    image: "https://github.com/zrCristian/prueba/blob/main/fotos/tevez.jpg?raw=true",
 
 })
-
 listaProductos.push({
     name: "computadora",
     price: 700,
-    image: "https://w0.peakpx.com/wallpaper/205/620/HD-wallpaper-messi-cute-messi-cute-argentina-messi-2021-messi-argentina-messi-2018-messi-argentina.jpg"
+    image: "https://resize.indiatvnews.com/en/resize/newbucket/730_-/2020/11/lionel-messi-diego-maradona-1604507732.jpg"
 })
-
-
-
-
+listaProductos.push({
+    name: "computadora",
+    price: 700,
+    image: "https://media.tycsports.com/files/2020/01/26/85005/bombonera.jpg"
+})
 
 
 // for (producto of listaProductos) {
@@ -126,6 +142,7 @@ listaProductos.push({
 //     cardsContainer.appendChild(productCard)
 // }
 
+
 listaProductos.forEach((e) => {
     const divProductCard = document.createRange().createContextualFragment(`
     <div class="product-card">
@@ -143,4 +160,14 @@ listaProductos.forEach((e) => {
     `);
 
     cardsContainer.appendChild(divProductCard)
+})
+    
+const productDetailIMG = document.querySelector("#productosDetalles > img")
+const productImg = document.querySelectorAll(".product-card > img") 
+
+productImg.forEach( (productos, indice) => {
+    productos.addEventListener("click", () => {
+            productDetailIMG.src = listaProductos[indice].image
+        })
+    productos.addEventListener("click", openProductDetailAside)
 })
